dfa22990632539f6187c919b3cb11f27f176074c
Tobitototo24 <tobiasruffino8@hotmail.com>
2025-10-01 12:44:13 -0300

    Update calculos.py

--- calculos.py
+++ calculos.py
@@ -45,18 +45,6 @@ def mejor_marca_por_categoria(lista, deporte, distancia):
             categorias[categoria] = None
 
     return categorias
-#aca se guardan los ganadores por categoria
-        
-    # funcion que muestra las mejores marcas por categoria y distancia:
-    # la idea es usar el fritro filtrar_por_edad() luego usar condicionales y guardar en un diccionario los datos {categoria, atleta}
-    # Ejemplo:
-    # paso 1: Definir un diccionario con las categorias = {"Sub-20": [], "Sub-30": [], "Sub-40": [], "Mayores de 40": []}
-    # paso 2: Filtrar atletas por deporte y distancia con la funcion: filtrar_por_edad
-    # paso 3: Buscar la mejor marca por categoría: guardar los ganadores en el dic ej: categorias["Sub-20"].append(atleta)
-    # paso 3.5: recordar que si no hay un atleta en la categoria mejores[categoria] = None
-    # usar la funcion elegir_edades() que esta en el archivo utils si fuese necesario
-    # Tobias
-    pass
 
 
 def calcular_3(lista, deporte, distancia):
